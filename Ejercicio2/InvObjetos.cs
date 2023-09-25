ec30cd10afcf9cf24a06b1964a21e7a9cecc4ae4
PascoF <113253733+PascoF@users.noreply.github.com>
2023-09-24 20:12:37 -0500

    InventarioObjetos

---
+++ Ejercicio2/InvObjetos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Invobjetos
+    {
+        protected string nombre;
+        protected int precio;
+        protected Invobjetos(string nombre, int precio)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+        }
+        public string ObtenernmbrObjeto()
+        {
+            return nombre;
+        }
+    }
+}
