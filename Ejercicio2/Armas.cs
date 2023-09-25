51a6d866f486cf137997ef7624ea162b63824b2c
luis8801 <103527495+luis8801@users.noreply.github.com>
2023-09-25 16:00:26 -0500

    tipos de armas

---
+++ Ejercicio2/Armas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    abstract class Armas : Tienda
+    {
+        public override string name { get; set; }
+        public float damage;
+        public float speedAtackked;
+        public override float precio { get; set; }
+
+        public override float PrecioTotal()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Ejercicio2/ArmasCuerpoaCuerpo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ejercicio2
+{
+    class ArmasCuerpoaCuerpo : Armas, IData
+    {
+        public ArmasCuerpoaCuerpo(string name, float damage, float speedAtackked, float precio)
+        {
+            this.name = name;
+            this.damage = damage;
+            this.speedAtackked = speedAtackked;
+            this.precio = precio;
+        }
+
+        public float DamageForSecond()
+        {
+            return damage / speedAtackked;
+        }
+        public override float PrecioTotal()
+        {
+            return precio;
+        }
+
+
+        public string Data()
+        {
+            return $"{name} - daño: {damage} - velocidad de ataque: {speedAtackked} - precio: {PrecioTotal()}";
+        }
+    }
+}
