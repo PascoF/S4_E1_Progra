df1a17f7e968de2f78886472e32467099477187b
PascoF <113253733+PascoF@users.noreply.github.com>
2023-09-26 08:49:34 -0500

    .

---
+++ Ejercicio2/ArmaduraMetal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class ArmaduraMetal : Armaduras
+    {
+        public ArmaduraMetal(string nombre, float reducciondano, float durabilidad, int precio) : base("ArmaduraMetal", reducciondano, durabilidad, precio)
+        {
+            this.nombre = nombre;
+        }
+        public float ReduccionDano()
+        {
+            return reducciondano;
+        }
+        public string GetData()
+        {
+            return $"{nombre} / La reduccion de daño de la armadura es... {reducciondano} / su durabilidad es {durabilidad} / Con todo esto, el precio de la armadura es... {precio}";
+        }
+    }
+}
