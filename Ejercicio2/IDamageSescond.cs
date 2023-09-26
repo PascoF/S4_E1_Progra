687993505d99705077746ad5081d8b8d3a3feeb4
luis8801 <103527495+luis8801@users.noreply.github.com>
2023-09-25 22:53:33 -0500

    tipos de armas y daño

---
+++ Ejercicio2/IDamageSescond.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    interface IDamageSescond
+    {
+        float DamageForSecond();
+
+    }
+    interface IData
+    {
+        string Data();
+    }
+}
