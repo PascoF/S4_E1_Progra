4c7d90755c8ba58f9caffd4d2fa82cd4a99e372a
luis8801 <103527495+luis8801@users.noreply.github.com>
2023-09-25 22:54:59 -0500

    proyectil para arma a distancia

---
+++ Ejercicio2/Proyectiles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Proyectiles : Armas
+    {
+
+
+        public Proyectiles(string name, float damage, float precio)
+        {
+            this.name = name;
+            this.damage = damage;
+            this.precio = precio;
+        }
+        public float Damage()
+        {
+            return damage;
+        }
+    }
+}
