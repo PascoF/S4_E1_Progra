246c6ee2387b5c1616f32fbc711694eeb0a43a01
luis8801 <103527495+luis8801@users.noreply.github.com>
2023-09-25 22:56:10 -0500

    tienda de armas y armaduras

---
+++ Ejercicio2/Tienda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    abstract class Tienda
+    {
+        public abstract float precio { get; set; }
+        public abstract string name { get; set; }
+
+        public abstract float PrecioTotal();
+
+
+    }
+}
